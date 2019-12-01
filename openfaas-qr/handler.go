a4d7239744965b9dcb4c4b4696fc9b03a8ed6ad7
droid42 <info@droid42.de>
2019-12-01 22:19:31 +0100

    qrcode

--- openfaas-qr/handler.go
+++ openfaas-qr/handler.go
@@ -1,20 +1,37 @@
 package function
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/openfaas-incubator/go-function-sdk"
+	"github.com/skip2/go-qrcode"
 )
 
 // Handle a function invocation
 func Handle(req handler.Request) (handler.Response, error) {
 	var err error
 
-	message := fmt.Sprintf("Hello world, input was: %s", string(req.Body))
+	var png []byte
+	png, err = qrcode.Encode(string(req.Body), qrcode.Medium, 256)
+	if err != nil {
+		return handler.Response{
+			Body:       []byte(err.Error()),
+			StatusCode: http.StatusInternalServerError,
+		}, err
+	}
+
+	var header = http.Header{}
+	header.Add("Content-Type", "image/png")
 
 	return handler.Response{
-		Body:       []byte(message),
+		Header:     header,
+		Body:       png,
 		StatusCode: http.StatusOK,
 	}, err
+
+	//message := fmt.Sprintf("Hello world, input was: %s", string(req.Body))
+	//return handler.Response{
+	//	Body:       []byte(message),
+	//	StatusCode: http.StatusOK,
+	//}, err
 }
